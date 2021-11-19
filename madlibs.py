caf7fe9063d166b478274cf81e9a1437faa2c8b2
Meggin <megginsimon@gmail.com>
2021-11-19 16:17:23 +0000

    updated 11192021

--- madlibs.py
+++ madlibs.py
@@ -12,25 +12,17 @@ AWESOMENESS = [
     "awesome",
     "terrific",
     "fantastic",
-    "neato",
-    "fantabulous",
-    "wowza",
-    "oh-so-not-meh",
-    "brilliant",
-    "ducky",
-    "coolio",
     "incredible",
     "wonderful",
     "smashing",
     "lovely",
 ]
 
-
 @app.route("/")
 def start_here():
     """Display homepage."""
 
-    return "Hi! This is the home page."
+    return render_template("gotohello.html")
 
 
 @app.route("/hello")
@@ -61,33 +53,27 @@ def  show_madlib_form():
     """asks if wants to play game"""
 
     return render_template("game_form.html")
-    # game_interest = request.args.get("game_interest")
 
-    # if game_interest == "on":
-    #     return render_template("game_form.html")
-    # else:
-    #     return render_template("goodbye.html")
-
-@app.route("/game_play", methods=["POST"])
+@app.route("/game_play")
 def show_madlib_result():
 
-    color = request.form.get("color")
+    color = request.args.get("color")
 
-    noun = request.form.get("noun")
+    noun = request.args.get("noun")
 
-    person_madlib = request.form.get("person_madlib")
+    person_madlib = request.args.get("person_madlib")
 
-    adjective = request.form.get("adjective")
+    adjective = request.args.get("adjective")
 
-    animal = request.form.get("animal")
+    animal = request.args.get("animal")
 
-    shape = request.form.get("shape")
+    shape = request.args.get("shape")
 
-    sound = request.form.get("sound")
+    sound = request.args.get("sound")
 
-    place = request.form.get("place")
+    place = request.args.get("place")
     
-    return render_template(choice(['game.html', 'game1.html']), color=color, noun=noun, person_madlib=person_madlib, adjective=adjective, animal=animal, shape=shape, sound=sound, place=place)
+    return render_template(choice(['game.html', 'game1.html', 'game2.html']), color=color, noun=noun, person_madlib=person_madlib, adjective=adjective, animal=animal, shape=shape, sound=sound, place=place)
 
 if __name__ == "__main__":
     # Setting debug=True gives us error messages in the browser and also
